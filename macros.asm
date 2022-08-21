444b69babb7260234aab7af63aa4eab65e4b8479
RAMPKORV <patrik.weibull@gmail.com>
2022-08-21 12:04:38 +0200

    Indentation fix

--- macros.asm
+++ macros.asm
@@ -1,48 +1,30 @@
 txt macro
-
 escape = 0
 i = 0
 	while i<strlen(\1)
 c substr 1+i,1+i,\1
 	if escape=0
-		; Escape character
 		if "\c"="\"
 escape = 1
-		; Space
 		elseif ("\c"=" ")
 			dc.b	$32
-
-                elseif ("\c"=".")
-                        dc.b    $29
-
-                elseif ("\c"="'")
-                        dc.b    $26
-
-                elseif ("\c"="!")
-                        dc.b    $2D
-
-                elseif ("\c"="?")
-                        dc.b    $28
-
-		; 0-9
+		elseif ("\c"=".")
+				dc.b    $29
+		elseif ("\c"="'")
+				dc.b    $26
+		elseif ("\c"="!")
+				dc.b    $2D
+		elseif ("\c"="?")
+				dc.b    $28
 		elseif ("\c">="0")&("\c"<="9")
 			dc.b	("\c"-"0")
-
-		; A-Z
 		elseif ("\c">="A")&("\c"<="Z")
 			dc.b	("\c"-"A")+$0A
-
-		; Small "x" (large font)
-		elseif ("\c"="x")
-			dc.b	$4A
-
-
 		endif
 	else
-		; Custom character
+		; newline
 		if "\c"="n"
 			dc.b	$FC
-		; Invalid
 		else
 			inform 2,"Invalid escape character '%s'", "\c"
 		endif
@@ -50,10 +32,8 @@ escape = 0
 	endif
 i = i+1
 	endw
-
     rept narg-1
 		shift
 		dc.b \1
 	endr
-
 	endm
