444f596340139fe6f83d5bf8b6decf20f5ad17d8
wasd <wasdsweden@gmail.com>
2022-09-18 12:26:17 +0200

    Build on unix systems

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+# SN 68k version 2.53, same version as asm68k.exe in repo
+# https://github.com/rhargreaves/asm68k-docker-image/blob/master/Dockerfile
+docker run --rm -v $(pwd):/src rhargreaves/asm68k /k /p /o ae- smgp.asm,out.bin
+
+echo
+./validate.sh
